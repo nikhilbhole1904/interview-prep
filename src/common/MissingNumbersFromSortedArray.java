2f729c85a101792eb1725268ad1329f6243a3b51
nikhilbhole1904 <nikhilbhole1904@gmail.com>
2024-08-14 22:27:51 +0530

    Update MissingNumbersFromSortedArray.java

--- src/common/MissingNumbersFromSortedArray.java
+++ src/common/MissingNumbersFromSortedArray.java
@@ -1,1 +1,30 @@
+package common;
+
+/**
+ * This solution provides missing values between sorted arrays.
+ * It's solved using two pointers technique.
+ */
+public class SortedArrayMissingNumbers {
+
+    public static void find(int[] numbers) {
+        int left = 0;
+        for (int right = 1; right < numbers.length; right++) {
+            if (numbers[left] + 1 != numbers[right]) {
+                int diff = numbers[right] - numbers[left];
+                int increment = 1;
+                while (diff > 1) {
+                    System.out.println(numbers[left] + increment);
+                    diff--;
+                    increment++;
+                }
+            } left++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,4,6,8,13};
+
+        find(numbers);
+    }
+}
 
